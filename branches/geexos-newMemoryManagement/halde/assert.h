864cea779cfbc1a31562da17c6ebce1ea7628d82
Andreas Galauner <andreas@galauner.de>
2009-03-19 04:48:42 +0000

    Added the old, removed sourcecode for perhaps later use to directory halde
    git-svn-id: https://svn.galauner.de/GeexOS@28 5530181a-2ae2-423d-a44f-b903e3aa7be7

---
+++ branches/geexos-newMemoryManagement/halde/assert.h
@@ -0,0 +1,9 @@
+#ifndef _ASSERT_H
+#define _ASSERT_H
+
+#include <stdint.h>
+
+#define ASSERT(b) ((b) ? (void)0 : panic_assert(__FILE__, __LINE__, #b))
+void panic_assert(const char *file, unsigned line, const char *desc);
+
+#endif
