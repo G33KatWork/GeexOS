d9c42f5dd2ad4e2ee62561250de18483c90d58a5
Andreas Galauner <andreas@galauner.de>
2009-04-07 05:23:13 +0000

    - Cleared the rest of the screen - Modified the Makefile to shut up the mount operations
    git-svn-id: https://svn.galauner.de/GeexOS@43 5530181a-2ae2-423d-a44f-b903e3aa7be7

--- branches/geexos-D/kernel/main.d
+++ branches/geexos-D/kernel/main.d
@@ -5,4 +5,8 @@ extern(C) void main(uint magic, uint addr)
         byte *videoram = cast( byte * ) 0xb8000; // Set up video memory
         videoram[0] = 'D';
         videoram[1] = 0x07;
+        
+        //clear the rest
+        for(int i = 0; i < 80*24*2 - 2; i++)
+            videoram[i+2] = 0;
 }
