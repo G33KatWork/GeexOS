85254ecb9cec237ccaad31d3bf75f647267ae4d5
Andreas Galauner <andreas@galauner.de>
2010-10-13 17:14:49 +0200

    Added abs() to libkcpp

---
+++ kernel/libkcpp/include/stdlib.h
@@ -0,0 +1,16 @@
+#ifndef _STDLIB_H
+#define _STDLIB_H
+
+#include <types.h>
+
+//namespace Lib
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    int abs(int i);
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ kernel/libkcpp/src/stdlib.cpp
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+
+int abs(int i)
+{
+    if(i < 0)
+        return -i;
+    return i;
+}
