b1eb13563e29c55ef7d66e81a650aee3361aedf9
Andreas Galauner <andreas@galauner.de>
2012-06-09 06:58:52 +0200

    Added strrchr and strncpy

--- bootloader/stage2/stage2/include/lib.h
+++ bootloader/stage2/stage2/include/lib.h
@@ -23,9 +23,11 @@ size_t strlen(const char *str);
 char tolower(char c);
 
 char* strcpy(char *dest, const char *src);
+char* strncpy(char *s1, const char *s2, size_t n);
 int strcmp(const char *s1, const char *s2);
 int stricmp(const char *s1, const char *s2);
 char* strchr(const char *s, int c);
+char* strrchr(const char *s, int c);
 void* memcpy(void *dest, const void* src, size_t count);
 void* memmove(void *dest, const void* src, size_t count);
 int memcmp(const void *s1, const void *s2, size_t len);
--- bootloader/stage2/stage2/src/lib.c
+++ bootloader/stage2/stage2/src/lib.c
@@ -58,6 +58,19 @@ char* strcpy(char *dest, const char *src)
 	return dest;
 }
 
+char* strncpy(char *s1, const char *s2, size_t n)
+{
+	char *dest = s1;
+	
+	for (; *s2 && n; n--)
+		*s1++ = *s2++;
+	
+	while (n--)
+		*s1++ = 0;
+	
+	return dest;
+}
+
 int strcmp(const char *s1, const char *s2)
 {
 	while (*s1 != 0 && *s1 == *s2)
@@ -90,6 +103,20 @@ char* strchr(const char *s, int c)
 			return 0;
 		s++;
 	}
+}
+
+char* strrchr(const char *s, int c)
+{
+	size_t i = 0;
+	while (s[i] != 0)
+		i++;
+
+	do
+		if (s[i] == c)
+			return (char *) s + i;
+	while (i-- != 0);
+
+	return 0;
 }
 
 void* memcpy(void *dest, const void* src, size_t count)
